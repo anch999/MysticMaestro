2bf6eeea4cda97c74c04f6a1567233b3f794ae24
Anch <adrianpratt.ap@gmail.com>
2023-01-17 19:21:27 +1300

    Revert "Revert "add PriceCorrection in selected enchant post logic""
    This reverts commit bf5a2f094e184126db8c23d20376adc905aab8f2.

--- MysticMaestro/automation/functions/scan.lua
+++ MysticMaestro/automation/functions/scan.lua
@@ -1,1 +1,44 @@
 local MM = LibStub("AceAddon-3.0"):GetAddon("MysticMaestro")
+
+local automationName = "Scan"
+
+local isPaused
+
+local automationTable = {}
+
+function automationTable.GetName()
+  return automationName
+end
+
+function automationTable.ShowInitPrompt()
+  print("showinitprompt called")
+  MM.AutomationUtil.ShowAutomationPopup(automationName, automationTable, "prompt")
+end
+
+
+
+function automationTable.Start()
+  print("start called")
+  -- collect list of enchants to search if not paused
+  -- show progress bar (set automation table, and make buttons visible based on whether or not automation is pausable, set progress current and max)
+  -- update progress bar
+  MM.AutomationUtil.ShowAutomationPopup(automationName, automationTable, "running")
+  isPaused = false
+end
+
+function automationTable.Pause()
+  print("pause called")
+  isPaused = true
+end
+
+function automationTable.IsPaused()
+  return isPaused
+end
+
+function automationTable.Stop()
+  print("stop called")
+  MM.AutomationUtil.HideAutomationPopup()
+  isPaused = false
+end
+
+MM.AutomationManager:RegisterAutomation(automationName, automationTable)
