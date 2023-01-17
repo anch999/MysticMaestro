355187b0c2e0ed5663e5599552261dd158358009
Anch <adrianpratt.ap@gmail.com>
2023-01-17 19:25:48 +1300

    Revert "Start writing automation util functions"
    This reverts commit 8bb29e2a3c444b8e3ee01c5488441e110fb8f6a5.

--- MysticMaestro/automation/functions/scan.lua
+++ MysticMaestro/automation/functions/scan.lua
@@ -1,11 +1,1 @@
 local MM = LibStub("AceAddon-3.0"):GetAddon("MysticMaestro")
-
-local scan = {
-  ShowInitPrompt = function() end,
-  Start = function() end,
-  Stop = function() end
-}
-
-scan.Options = {}
-
-MM.AutomationManager:RegisterAutomation("Scan", scan)
